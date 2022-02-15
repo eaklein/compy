404a7c7fc869a7a4357a03ff9005ec8687a618dd
eaklein <eklein@mit.edu>
2022-02-15 13:52:14 -0500

    changed directory structure and updated README

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-* COMPY
+# CoMPy: The CoMPASS Python Companion
+
+## Introduction
+
+CoMPy is a Python package for easily accessing and analyzing data acquired using the CAEN Multi-PArameter Spectroscopy Software (COMPASS).
+
+## Tutorial
+
+To be added later.
--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="compy-EAKLEIN",
-    version="0.0.1",
+    name="CoMPy",
+    version="1.0",
     author="E. A. Klein",
     author_email="eklein@mit.edu",
     description="A package for reading CoMPASS directories into Python",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/eaklein/CoMPASS",
+    url="https://github.com/eaklein/CoMPy",
     project_urls={
-        "Bug Tracker": "https://github.com/eaklein/CoMPASS/issues",
+        "Bug Tracker": "https://github.com/eaklein/CoMPy/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
