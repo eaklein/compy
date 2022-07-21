f180946685dc5788f3128bffab5ce846137ea24d
E. A. Klein <eklein@mit.edu>
2022-07-20 20:28:14 -0400

    update version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="compy",
-    version="1.0",
+    version="1.0.1",
     author="E. A. Klein",
     author_email="eklein@mit.edu",
     description="A package for reading CoMPASS directories into Python",
